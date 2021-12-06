5740998bd690632b232d4caa420ca3a6b69d9a48
reena <reena040@gmail.com>
2021-12-06 10:14:52 -0500

    added extra space for comments

--- Mlogin/ViewController.swift
+++ Mlogin/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var buttonText: UIButton!
+    @IBOutlet weak var userText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        userText.layer.cornerRadius = 22
+        passText.layer.cornerRadius = 22
+        buttonText.layer.cornerRadius = 22
+        
+      //
+        //
+        //
+        //
+        //
+        
+        
     }
 
 
